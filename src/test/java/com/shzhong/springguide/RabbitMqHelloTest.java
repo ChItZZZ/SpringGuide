22062094297fa623830f9f9629470778651dc32c
chitzzz <648267970@qq.com>
2018-12-12 23:58:36 +0800

    first commit

---
+++ src/test/java/com/shzhong/springguide/RabbitMqHelloTest.java
@@ -0,0 +1,36 @@
+package com.shzhong.springguide;
+
+import com.shzhong.springguide.service.HelloSender;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitMqHelloTest {
+
+    @Autowired
+    private HelloSender helloSender;
+
+    @Test
+    public void hello() throws Exception {
+        helloSender.send();
+    }
+
+    @Test
+    public void oneToMany() throws Exception {
+        for (int i = 0; i < 100; i++) {
+            helloSender.send();
+        }
+    }
+
+    @Test
+    public void manyToMany() throws Exception {
+        for (int i = 0; i < 100; i++) {
+            helloSender.send();
+            helloSender.send();
+        }
+    }
+}
